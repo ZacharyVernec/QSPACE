6931ffc84ac6bf77c418cc9c16e086b7c4053ee7
EloiseChakour <eloise.chakour@gmail.com>
2018-11-03 16:29:58 -0400

    Updated Code (EC)
    Made functions & added random behaviour. - EC

--- QSPACE/Workshop.py
+++ QSPACE/Workshop.py
@@ -9,35 +9,54 @@ Created on Sat Nov  3 12:41:10 2018
 import numpy as np
 import random as rand
 
+
+#Initial Useful variables
 expected_average = 6
 n = 10
 
+#Define a test array that will be imported later
 valency = np.zeros[n]
 
 for i in range[len(valency)]:
     valency[i] = n-1
 
 
-for i in range[len(valency)]:
+
+
+#Picking an edge to remove
     
+    
+#Checks if there are any edges left to remove
+def checkContinue():
     remove = 1
+    for i in range[len(valency)]:
+        if valency[i] > expected_average:
+            remove ==1
+        else: 
+            remove = 0
     
-    if valency[i] > expected_average:
-        remove ==1
-    else: 
-        remove = 0
-        
+    return remove
     
     
-    while remove == 1:
-        eligible_vertices = []
+#Makes the picking of the edge somewhat random
+def pickRandEdge(eligible_vertices):
+     edge_address = rand.randint(0, len(eligible_vertices))
+     edge_number = eligible_vertices[edge_address]
+     return edge_number
+                
+    
+#Tells you which edge you're removing    
+def pickEdge(remove):
+    eligible_vertices = []
         
-        for j in range[len(valency)]:
-            if valency[j] > expected_average:
-                eligible_vertices.append[j]
+    for j in range[len(valency)]:
+        if valency[j] > expected_average:
+            eligible_vertices.append[j]
         
-        
-        
+    edge = pickRandEdge(eligible_vertices)
+    return edge
+
+
 
 
 
