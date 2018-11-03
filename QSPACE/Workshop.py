41c635bcbc5a39bbc61d3158d5575cc81dc22486
Zachary Vernec <zachary.vernec@gmail.com>
2018-11-03 18:20:07 -0400

    Added adjacency matrix, and bug fixes

--- QSPACE/Workshop.py
+++ QSPACE/Workshop.py
@@ -2,12 +2,10 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Nov  3 12:41:10 2018
-
 @author: eloisechakour
 """
-
 import numpy as np
-import random as rand
+numpy.random.seed(0)
 
 
 #Initial Useful variables
@@ -15,110 +13,71 @@ expected_average = 6
 n = 10
 
 #Define test arrays that will be imported later
-def valFct(n):
-    valency = np.zeros[n]
-
-    for i in range[len(valency)]:
-        valency[i] = n-1
+def initValMat(n):
+    adjmat = np.ones((n, n), dtype=int)
+    for i in range(n):
+        adjmat[i, i] = 0
+    return adjmat
     
-    return valency
-    
-smallPointArray = []
-bigPointArray = []
-sliceOfMatrix = []
-adjMatrix = []
-
-
+def getValArr(adjmat):
+    valArr = np.empty((n,))
+    for i in range(n):
+        valArr[i] = np.sum(adjmat[i], dtype=int)
+    return valArr
 
 
 #Picking an edge to remove
     
     
 #Checks if there are any edges left to remove
-def checkContinue():
-    remove = 1
-    for i in range[len(valency)]:
-        if valency[i] > expected_average:
-            remove ==1
-        else: 
-            remove = 0
-    
-    return remove
+def checkIfNotDone(adjmat):
+    valArr = getValArr(adjmat)
+    avgVal = np.sum(valArr)/n
+    if avgVal > expected_average:
+        return True
+    else:
+        return False
     
     
 #Makes the picking of the vertex somewhat random
 def pickRandVertex(eligible_vertices):
-     vertex_address = rand.randint(0, len(eligible_vertices))
+     vertex_address = np.random.randint(0, len(eligible_vertices))
      vertex_number = eligible_vertices[vertex_address]
      return vertex_number
                 
     
 #Tells you which vertex you're removing an edge from
-def pickVertex(valency):
+def pickVertex(valArr):
     eligible_vertices = []
         
-    for j in range[len(valency)]:
-        if valency[j] > expected_average:
-            eligible_vertices.append[j]
+    for i in range(n):
+        if valArr[i] > expected_average:
+            eligible_vertices.append(i)
         
     vertex = pickRandVertex(eligible_vertices)
     return vertex
 
 
 #Pick an edge from the selected vertex
-def pickEdge(vertex, sliceOfMatrix, valency):
-    foundLine = 0
-    while foundLine ==0:
-        i = 0
-        if sliceOfMatrix[i] == 1:
-            if valency[i] > expected_average:
-                valency[vertex] -=1
-                valency[i] -= 1
-                sliceOfMatrix[i] = 0
-                break
+#RETURNS a new matrix with the edge removed
+def removeAnEdge(vertex, adjmat):
+    valArr = getValArr(adjmat)
+    foundLine = False
+    i = 0
+    while not foundline:
+        if valArr[i] > expected_average:
+            adjmat[vertex, j] = 0
+            adjmat[j, vertex] = 0
+            foundline = True
         else:
             i+=1
+    return adjmat
     
 
-def executable(adjMatrix, expected_Average, n):
-    valency = valFct(n)
-    remove = checkContinue()
-    while remove ==1:
-        vertex = pickVertex(valency)
-        #this is probably wrong
-        sliceOfMatrix = adjMatrix[vertex]
-        pickEdge(vertex, sliceOfMatrix, valency)
-        adjMatrix[vertex] = sliceOfMatrix
-    
+def executable(n, expected_Average):
+    adjMatrix = getValMat(n)
+    valArr = getValArr(adjmat)
+    while checkIfNotDone == True:
+        vertexToRemove = pickVertex(valArr)
+        adjMatrix = removeAnEdge(vertexToRemove)
     return adjMatrix
-        
-
-
-
-
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-
-
-
-
-
