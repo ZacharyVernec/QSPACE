f07f29c7185eebb4feeeec1212779bcaba7e46ad
EloiseChakour <eloise.chakour@gmail.com>
2018-11-03 16:16:13 -0400

    Uploaded Code (EC)

---
+++ QSPACE/Workshop.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov  3 12:41:10 2018
+
+@author: eloisechakour
+"""
+
+import numpy as np
+import random as rand
+
+expected_average = 6
+n = 10
+
+valency = np.zeros[n]
+
+for i in range[len(valency)]:
+    valency[i] = n-1
+
+
+for i in range[len(valency)]:
+    
+    remove = 1
+    
+    if valency[i] > expected_average:
+        remove ==1
+    else: 
+        remove = 0
+        
+    
+    
+    while remove == 1:
+        eligible_vertices = []
+        
+        for j in range[len(valency)]:
+            if valency[j] > expected_average:
+                eligible_vertices.append[j]
+        
+        
+        
+
+
+
+
+
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+
+
+
+
