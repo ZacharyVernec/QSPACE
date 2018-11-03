c2e26cc86b83f807babb8b7683c0f222ad9f586e
EloiseChakour <eloise.chakour@gmail.com>
2018-11-03 16:56:49 -0400

    First Draft of Edge Removing Alg - EC

--- QSPACE/Workshop.py
+++ QSPACE/Workshop.py
@@ -14,11 +14,19 @@ import random as rand
 expected_average = 6
 n = 10
 
-#Define a test array that will be imported later
-valency = np.zeros[n]
+#Define test arrays that will be imported later
+def valFct(n):
+    valency = np.zeros[n]
 
-for i in range[len(valency)]:
-    valency[i] = n-1
+    for i in range[len(valency)]:
+        valency[i] = n-1
+    
+    return valency
+    
+smallPointArray = []
+bigPointArray = []
+sliceOfMatrix = []
+adjMatrix = []
 
 
 
@@ -38,28 +46,52 @@ def checkContinue():
     return remove
     
     
-#Makes the picking of the edge somewhat random
-def pickRandEdge(eligible_vertices):
-     edge_address = rand.randint(0, len(eligible_vertices))
-     edge_number = eligible_vertices[edge_address]
-     return edge_number
+#Makes the picking of the vertex somewhat random
+def pickRandVertex(eligible_vertices):
+     vertex_address = rand.randint(0, len(eligible_vertices))
+     vertex_number = eligible_vertices[vertex_address]
+     return vertex_number
                 
     
-#Tells you which edge you're removing    
-def pickEdge(remove):
+#Tells you which vertex you're removing an edge from
+def pickVertex(valency):
     eligible_vertices = []
         
     for j in range[len(valency)]:
         if valency[j] > expected_average:
             eligible_vertices.append[j]
         
-    edge = pickRandEdge(eligible_vertices)
-    return edge
+    vertex = pickRandVertex(eligible_vertices)
+    return vertex
 
 
+#Pick an edge from the selected vertex
+def pickEdge(vertex, sliceOfMatrix, valency):
+    foundLine = 0
+    while foundLine ==0:
+        i = 0
+        if sliceOfMatrix[i] == 1:
+            if valency[i] > expected_average:
+                valency[vertex] -=1
+                valency[i] -= 1
+                sliceOfMatrix[i] = 0
+                break
+        else:
+            i+=1
+    
 
-
-
+def executable(adjMatrix, expected_Average, n):
+    valency = valFct(n)
+    remove = checkContinue()
+    while remove ==1:
+        vertex = pickVertex(valency)
+        #this is probably wrong
+        sliceOfMatrix = adjMatrix[vertex]
+        pickEdge(vertex, sliceOfMatrix, valency)
+        adjMatrix[vertex] = sliceOfMatrix
+    
+    return adjMatrix
+        
 
 
 
