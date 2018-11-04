02f7e1d7984ccb485125b55a2327345639d08149
EloiseChakour <eloise.chakour@gmail.com>
2018-11-04 02:06:13 -0500

    Updated stats

--- QSPACE/stat.py
+++ QSPACE/stat.py
@@ -6,34 +6,63 @@ Created on Sat Nov  3 20:27:07 2018
 @author: eloisechakour
 """
 
-#Have valancy
+#Have Adj Matrix
 
-valancy = []
+#valancy = []
 n=10
+
+#Create Valency Array
+def valencyArr(n, adjMatrix):
+    valencies = []
+    val = 0
+    for i in range(n):
+        for j in range(n):
+            val += adjMatrix[i, j]
+        valencies.append(val)
+    
+    
+    
+    return valencies
+
 
 
 #Compute total energy
 
-def energy(valency, n):
+def energy(valencies, n):
     energy = 0
     for i in range(n):
-        energy += (valency[i])**2
+        energy += (valencies[i])**2
+    normEnergy = energy - 36*(n**2)
+    return normEnergy
     
-    return energy
-    
-
 #Compute Average Valency
     
 
-def avgVal(valency, adjArr):
+def avgVal(valency, adjMatrix):
     edges = 0
     for i in range(n):
         for j in range(n):
-            edges += adjArr[i, j]
+            edges += adjMatrix[i, j]
             #implicitly computes factor of 2
     
     alpha = edges/n
     
     return alpha
+
+
+#Change in distance 
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
