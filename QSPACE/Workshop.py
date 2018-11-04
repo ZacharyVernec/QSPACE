86c87457a40e039d9820c36c75b129f2085e2b71
1sadtrombone <taj.dyson@gmail.com>
2018-11-03 21:51:59 -0400

    Update Workshop.py

--- QSPACE/Workshop.py
+++ QSPACE/Workshop.py
@@ -4,14 +4,12 @@
 Created on Sat Nov  3 12:41:10 2018
 @author: eloisechakour
 """
-import time
 import numpy as np
 np.random.seed(0)
 
 
 #Initial Useful variables
 expected_average = 6
-n = 10
 
 #Define test arrays that will be imported later
 def initValMat(n):
@@ -21,6 +19,7 @@ def initValMat(n):
     return adjmat
     
 def getValArr(adjmat):
+    n = len(adjmat[0])
     valArr = np.empty((n,))
     for i in range(n): 
         valArr[i] = n-1
@@ -51,6 +50,7 @@ def pickRandVertex(eligible_vertices):
     
 #Tells you which vertex you're removing an edge from
 def pickVertex(valArr):
+    n = len(valArr)
     eligible_vertices = []
         
     for i in range(n):
@@ -60,28 +60,6 @@ def pickVertex(valArr):
     vertex = pickRandVertex(eligible_vertices)
     return vertex
 
-'''
-#Pick an edge from the selected vertex
-#RETURNS a new matrix with the edge removed
-def removeAnEdge(vertex, adjmat, valArr):
-    stop = 0
-    i = 0
-    
-    while adjmat[vertex, i] == 1 and stop == 0:
-        #print(vertex)   
-        print("still in loop here...")        
-        if valArr[i] > expected_average:
-            adjmat[vertex, i] = 0
-            adjmat[i, vertex] = 0
-            valArr[vertex] -=1
-            valArr[i] -=1
-            stop = 1
-        else:
-            print("in else case")
-            i+=1
-    return adjmat
-
-''' 
 def removeAnEdge(vertex, adjmat, valArr):
     row = adjmat[vertex]
     for i in range(0, len(row)-1):
@@ -93,7 +71,7 @@ def removeAnEdge(vertex, adjmat, valArr):
             return adjmat
         
 
-def executable(n, expected_Average):
+def executable(n):
     adjMatrix = initValMat(n)
     valArr = getValArr(adjMatrix)
     adjMatricesOverTime = [adjMatrix]
@@ -105,30 +83,8 @@ def executable(n, expected_Average):
         running = checkIfNotDone(adjMatrix2, valArr)
         #print("Continue Variable = " + str(running))
         print(valArr)
-    return adjMatricesOverTime, print("Done!")
+    print(adjMatricesOverTime[-1])
+    return adjMatricesOverTime
 
 
-matrices = executable(n, expected_average)
-
-def toGEXF(adjMatricesOverTime):
-    with open('startGEXF.txt', 'r') as template:
-        startstr = template.read()
-    nowstr = str(int(time.time()))
-    
-    endoftheworld = len(adjMatricesOverTime)
-    with open(nowstr, 'w') as file:
-        file.write(startstr)
-        file.write('    <nodes>')
-        for i in range(n):
-            file.write('      <node id="'+str(i)+'" label="Node '+str(i)+'" start="0" endopen="'+str(endoftheworld)+'" />')
-        file.write('    </nodes>')
-        file.write('    <edges>')
-        edgeEndTimes = sum(adjMatricesOverTime)
-        for i in range(n):
-            for j in range(n):
-                if j >= i: #check if in upper triangle
-                    file.write('<edge source="'+str(i)+'" target="'+str(j)+'" start="0" endopen="'+str(edgeEndTimes[i, j])+'"/>')
-        file.write('    </edges>')
-        file.write('  </graph>')
-        file.write('</gexf>')
-toGEXF(matrices)
+finalMatrix = executable(10)
