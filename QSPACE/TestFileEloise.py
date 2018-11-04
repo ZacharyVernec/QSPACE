a2c9bbabb9aab3a396415b7b45f12f9d3a52594c
EloiseChakour <eloise.chakour@gmail.com>
2018-11-04 02:32:04 -0500

    Finished stats (Actually)

---
+++ QSPACE/TestFileEloise.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov  3 12:41:10 2018
+@author: eloisechakour
+"""
+import numpy as np
+np.random.seed(2)
+
+
+#Initial Useful variables
+expected_average = 6
+
+#Define test arrays that will be imported later
+def initValMat(n):
+    adjmat = np.ones((n, n), dtype=int)
+    for i in range(n):
+        adjmat[i, i] = 0
+    return adjmat
+    
+def getValArr(adjmat):
+    n = len(adjmat[0])
+    valArr = np.empty((n,))
+    for i in range(n): 
+        valArr[i] = n-1
+    #for i in range(n):
+        #valArr[i] = np.sum(adjmat[i], dtype=int)
+    return valArr
+
+
+#Picking an edge to remove
+    
+    
+#Checks if there are any edges left to remove
+def checkIfNotDone(adjmat, valArr):
+    cont = 0
+    for i in range(len(valArr)):
+        if valArr[i] > expected_average:
+            cont = 1
+    
+    return cont
+    
+    
+#Makes the picking of the vertex somewhat random
+def pickRandVertex(eligible_vertices):
+     vertex_address = np.random.randint(0, len(eligible_vertices))
+     vertex_number = eligible_vertices[vertex_address]
+     return vertex_number
+                
+    
+#Tells you which vertex you're removing an edge from
+def pickVertex(valArr):
+    n = len(valArr)
+    eligible_vertices = []
+        
+    for i in range(n):
+        if valArr[i] > expected_average:
+            eligible_vertices.append(i)
+        
+    vertex = pickRandVertex(eligible_vertices)
+    return vertex
+
+def removeAnEdge(vertex, adjmat, valArr):
+    row = adjmat[vertex]
+    for i in range(0, len(row)-1):
+        if row[i] == 1 and valArr[i] > expected_average:
+            adjmat[vertex, i] = 0
+            adjmat[i, vertex] = 0
+            valArr[vertex] -=1
+            valArr[i] -=1
+            return adjmat
+        
+
+def executable(n):
+    adjMatrix = initValMat(n)
+    valArr = getValArr(adjMatrix)
+    adjMatricesOverTime = [adjMatrix]
+    running = 1
+    while running:
+        vertexToRemove = pickVertex(valArr)
+        adjMatrix2 = removeAnEdge(vertexToRemove, adjMatrix, valArr)
+        adjMatricesOverTime.append(adjMatrix2)
+        running = checkIfNotDone(adjMatrix2, valArr)
+        #print("Continue Variable = " + str(running))
+        print(valArr)
+    print(adjMatricesOverTime[-1])
+    return adjMatricesOverTime[-1]
+
+
+finalMatrix = executable(10)
--- QSPACE/stat.py
+++ QSPACE/stat.py
@@ -5,19 +5,25 @@ Created on Sat Nov  3 20:27:07 2018
 
 @author: eloisechakour
 """
+#import TestFileEloise as t
 
 #Have Adj Matrix
 
 #valancy = []
 n=10
 
+#Getting a test adjacency matrix - ignore this it's just to test stuff
+#adjMatrix = [[0, 1, 0, 1, 1, 1, 0, 1, 0, 1],[1, 0, 1, 0, 1, 1, 1, 1, 0, 0],[0, 1, 0, 0, 1, 1, 1, 1, 0, 1],[1, 0, 0, 0, 1, 1, 0, 1, 1, 1],[1, 1, 1, 1, 0, 0, 0, 0, 1, 1],[1, 1, 1, 1, 0, 0, 1, 0, 1, 0],[0, 1, 1, 0, 0 ,1 ,0 ,1, 1, 1],[1, 1, 1, 1, 0, 0, 1, 0, 1, 0],[0, 0, 0, 1, 1, 1, 1, 1, 0, 1],[1, 0, 1, 1, 1, 0, 1, 0, 1, 0]]
+
+
+
 #Create Valency Array
 def valencyArr(n, adjMatrix):
     valencies = []
-    val = 0
     for i in range(n):
+        val = 0
         for j in range(n):
-            val += adjMatrix[i, j]
+            val += adjMatrix[i][j]
         valencies.append(val)
     
     
@@ -32,7 +38,7 @@ def energy(valencies, n):
     energy = 0
     for i in range(n):
         energy += (valencies[i])**2
-    normEnergy = energy - 36*(n**2)
+    normEnergy = energy - 36*n
     return normEnergy
     
 #Compute Average Valency
@@ -42,7 +48,7 @@ def avgVal(valency, adjMatrix):
     edges = 0
     for i in range(n):
         for j in range(n):
-            edges += adjMatrix[i, j]
+            edges += adjMatrix[i][j]
             #implicitly computes factor of 2
     
     alpha = edges/n
@@ -61,6 +67,14 @@ def changeInDist(side1, side2, n):
     return deltaDist
 
 
+
+
+def executableStats(n, adjMatrix):
+    valencies = valencyArr(n, adjMatrix)
+    E = energy(valencies, n)
+    alpha = avgVal(valencies, adjMatrix)
+    avgDistChange = changeInDist(2, 4, n)
+    return E, alpha, avgDistChange
 
 
 
