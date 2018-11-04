2b875e466b4b6a3a36e4dc53e5eca34dd6e5ba62
EloiseChakour <eloise.chakour@gmail.com>
2018-11-03 20:39:42 -0400

    NewFile - EC

---
+++ QSPACE/stat.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov  3 20:27:07 2018
+
+@author: eloisechakour
+"""
+
+#Have valancy
+
+valancy = []
+n=10
+
+
+#Compute total energy
+
+def energy(valency, n):
+    
+    
+    return 
+    
+
+
+
+
